622cd96cb3a7abac733f6b5c61283decae73478e
Hollie Eggleton <71609323+hollieegg@users.noreply.github.com>
2021-03-02 18:51:56 -0500

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -131,7 +131,7 @@ define({
   //When true display a scalebar on the map
   "scalebar": false,
   //Specify a title for the application. If not provided the web map title is used.
-  "title": "Explore the Provinces and Terrotories of Canada",
+  "title": "Explore the Provinces and Territories of Canada",
   //Optionally specify some sub title text.
   "subtitle": null,
   "level": null,
